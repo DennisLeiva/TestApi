bbdf2582b9a7325666b9558926b067721770644f
Dennis Leiva <dleivabiometrika@gmail.com>
2022-05-03 18:37:33 -0500

    se elimina sim usar

---
+++ Controllers/CakeControl.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    public class CakeControl : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
